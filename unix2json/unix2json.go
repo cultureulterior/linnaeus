9d115dbf3ea59c4a28bf78205645112c3d5a93fb
cultureulterior <sam@firstbanco.com>
2014-11-06 17:05:20 +0000

    Fix up unix2json

--- unix2json/unix2json.go
+++ unix2json/unix2json.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"encoding/json"
-	"io/ioutil"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"strconv"
 )
 
 var out = flag.String("out", "-", "Outfile")
 
 func main() {
 	flag.Parse()
-	data:=make(map[string]string)
+	data := make(map[string]int64)
+	m_junk, err := ioutil.ReadFile("/proc/meminfo")
+	c_junk, err := ioutil.ReadFile("/proc/cpuinfo")
+	m_regex := regexp.MustCompile("MemTotal:[[:space:]]+([0-9]+)")
+	c_regex := regexp.MustCompile("^processor")
+	data["memory"], err = strconv.ParseInt(m_regex.FindStringSubmatch(string(m_junk))[1], 10, 64)
+	data["cpus"] = int64(len(c_regex.FindAllIndex(c_junk, -1)))
 	jsonString, err := json.Marshal(data)
 	if err != nil {
 		log.Fatal(err)
